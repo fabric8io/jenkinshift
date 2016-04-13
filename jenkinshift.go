4be86426ed3d0ffe019009b9eda9c1a054f9b7dc
James Strachan <james.strachan@gmail.com>
2016-04-13 09:59:25 +0100

    first spike of working REST API to get a list of BCs or a single BC from a Jenkins Job

--- jenkinshift.go
+++ jenkinshift.go
@@ -3,109 +3,41 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/emicklei/go-restful"
+	"github.com/fabric8io/golang-jenkins"
 	"github.com/fabric8io/jenkinshift/openshift"
 )
-
-type User struct {
-	Id, Name string
-}
-
-type UserResource struct {
-	// normally one would use DAO (data access object)
-	users map[string]User
-}
-
-func (u UserResource) Register(container *restful.Container) {
-	ws := new(restful.WebService)
-	ws.
-		Path("/users").
-		Consumes(restful.MIME_XML, restful.MIME_JSON).
-		Produces(restful.MIME_JSON, restful.MIME_XML) // you can specify this per route as well
-
-	ws.Route(ws.GET("/").To(u.findUsers))
-	ws.Route(ws.GET("/{user-id}").To(u.findUser))
-	ws.Route(ws.POST("").To(u.updateUser))
-	ws.Route(ws.PUT("/{user-id}").To(u.createUser))
-	ws.Route(ws.DELETE("/{user-id}").To(u.removeUser))
-
-	container.Add(ws)
-}
-
-// GET http://localhost:8080/users
-//
-func (u UserResource) findUsers(request *restful.Request, response *restful.Response) {
-	response.WriteEntity(u.users)
-}
-
-// GET http://localhost:8080/users/1
-//
-func (u UserResource) findUser(request *restful.Request, response *restful.Response) {
-	id := request.PathParameter("user-id")
-	usr := u.users[id]
-	if len(usr.Id) == 0 {
-		response.AddHeader("Content-Type", "text/plain")
-		response.WriteErrorString(http.StatusNotFound, "User could not be found.")
-	} else {
-		response.WriteEntity(usr)
-	}
-}
-
-// POST http://localhost:8080/users
-// <User><Id>1</Id><Name>Melissa Raspberry</Name></User>
-//
-func (u *UserResource) updateUser(request *restful.Request, response *restful.Response) {
-	usr := new(User)
-	err := request.ReadEntity(&usr)
-	if err == nil {
-		u.users[usr.Id] = *usr
-		response.WriteEntity(usr)
-	} else {
-		response.AddHeader("Content-Type", "text/plain")
-		response.WriteErrorString(http.StatusInternalServerError, err.Error())
-	}
-}
-
-// PUT http://localhost:8080/users/1
-// <User><Id>1</Id><Name>Melissa</Name></User>
-//
-func (u *UserResource) createUser(request *restful.Request, response *restful.Response) {
-	usr := User{Id: request.PathParameter("user-id")}
-	err := request.ReadEntity(&usr)
-	if err == nil {
-		u.users[usr.Id] = usr
-		response.WriteHeader(http.StatusCreated)
-		response.WriteEntity(usr)
-	} else {
-		response.AddHeader("Content-Type", "text/plain")
-		response.WriteErrorString(http.StatusInternalServerError, err.Error())
-	}
-}
-
-// DELETE http://localhost:8080/users/1
-//
-func (u *UserResource) removeUser(request *restful.Request, response *restful.Response) {
-	id := request.PathParameter("user-id")
-	delete(u.users, id)
-}
-
 
 func main() {
 	wsContainer := restful.NewContainer()
 	wsContainer.Router(restful.CurlyRouter{})
-	u := UserResource{map[string]User{
-			"1": User{
-				Id: "1",
-				Name: "James",
-			},
-		}}
-	u.Register(wsContainer)
 
-	bcr := openshift.BuildConfigsResource{}
+	jenkinsHost := os.Getenv("JENKINS_HOST")
+	if len(jenkinsHost) == 0 {
+		jenkinsHost = "jenkins"
+	}
+	jenkinsUrl := "http://" + jenkinsHost + "/"
+
+	log.Printf("Invoking Jenkins on URL %s", jenkinsUrl)
+
+	auth := &gojenkins.Auth{
+	   /*
+	   Username: "[jenkins user name]",
+	   ApiToken: "[jenkins API token]",
+	   */
+	}
+	jenkins := gojenkins.NewJenkins(auth, jenkinsUrl)
+
+
+	bcr := openshift.BuildConfigsResource{
+		JenkinsURL: jenkinsUrl,
+		Jenkins: jenkins,
+	}
 	bcr.Register(wsContainer)
 
-	log.Printf("start listening on localhost:9090")
+	log.Printf("jenkinshift start listening on localhost:9090")
 	server := &http.Server{Addr: ":9090", Handler: wsContainer}
 	log.Fatal(server.ListenAndServe())
 }
--- openshift/buildConfigs.go
+++ openshift/buildConfigs.go
@@ -1,63 +1,152 @@
 package openshift
 
 import (
-	/*
+	"fmt"
 	"log"
 	"net/http"
-	*/
 
 	"github.com/emicklei/go-restful"
+	"github.com/fabric8io/golang-jenkins"
+
 	kapi "k8s.io/kubernetes/pkg/api/v1"
 	oapi "github.com/openshift/origin/pkg/build/api/v1"
 )
 
 type BuildConfigsResource struct {
+	JenkinsURL	string
+	Jenkins 	*gojenkins.Jenkins
 }
 
-
-func (u BuildConfigsResource) Register(container *restful.Container) {
+func (r BuildConfigsResource) Register(container *restful.Container) {
 	ws := new(restful.WebService)
 	ws.
-	Path("/namespaces/{namespace}/buildConfigs").
+	Path("/namespaces/{namespace}/buildconfigs").
 	Consumes(restful.MIME_XML, restful.MIME_JSON).
 	Produces(restful.MIME_JSON, restful.MIME_XML)
 
-	ws.Route(ws.GET("/").To(u.findBuildConfigs))
+	ws.Route(ws.GET("/").To(r.getBuildConfigs))
+	ws.Route(ws.GET("/{name}").To(r.getBuildConfig))
 	/*
-	ws.Route(ws.GET("/{user-id}").To(u.findUser))
-	ws.Route(ws.POST("").To(u.updateUser))
-	ws.Route(ws.PUT("/{user-id}").To(u.createUser))
-	ws.Route(ws.DELETE("/{user-id}").To(u.removeUser))
+	ws.Route(ws.POST("").To(r.updateBuildConfig))
+	ws.Route(ws.PUT("/{name}").To(r.createBuildConfig))
+	ws.Route(ws.DELETE("/{name}").To(r.removeBuildConfig))
 	*/
-
 	container.Add(ws)
 }
 
-// GET http://localhost:8080/namespaces/{namespaces}/buildConfigs
+func respondError(request *restful.Request, response *restful.Response, err error) {
+	message := fmt.Sprintf("%s", err)
+	respondErrorMessage(request, response, message)
+}
+
+func respondErrorMessage(request *restful.Request, response *restful.Response, message string) {
+	response.AddHeader("Content-Type", "text/plain")
+	response.WriteErrorString(http.StatusNotFound, message)
+}
+
+// GET http://localhost:8080/namespaces/{namespaces}/buildconfigs
 //
-func (u BuildConfigsResource) findBuildConfigs(request *restful.Request, response *restful.Response) {
+func (r BuildConfigsResource) getBuildConfigs(request *restful.Request, response *restful.Response) {
 	ns := request.PathParameter("namespace")
 
-	buildConfigs := []oapi.BuildConfig{
-		oapi.BuildConfig{
-			ObjectMeta: kapi.ObjectMeta{
-				Name: "foo",
-				Namespace: ns,
-			},
-			Spec: oapi.BuildConfigSpec{
-				BuildSpec: oapi.BuildSpec{
-					Source: oapi.BuildSource{
-						Type: oapi.BuildSourceGit,
-						Git: &oapi.GitBuildSource{
-							URI: "http://github.com/foo/bar.git",
-						},
+	jenkins := r.Jenkins
+	jobs, err := jenkins.GetJobs()
+	if err != nil {
+		respondError(request, response, err)
+		return
+	}
 
-					},
-				},
-			},
-		},
+	buildConfigs := []*oapi.BuildConfig{}
+
+	for _, job := range jobs {
+		buildConfig, err := r.loadBuildConfig(ns, job.Name, job.Url)
+		if err != nil {
+			log.Printf("Failed to find job %s due to %s", job.Name, err)
+		} else if buildConfig != nil {
+			buildConfigs = append(buildConfigs, buildConfig)
+		}
 	}
 	response.WriteEntity(buildConfigs)
 }
 
+// GET http://localhost:8080/namespaces/{namespaces}/buildconfigs/{name}
+//
+func (r BuildConfigsResource) getBuildConfig(request *restful.Request, response *restful.Response) {
+	ns := request.PathParameter("namespace")
+	jobName := request.PathParameter("name")
+	if len(jobName) == 0 {
+		respondErrorMessage(request, response, "No BuildConfig name specified in URL")
+		return
+	}
+	jobUrl := r.Jenkins.GetJobUrl(jobName)
+
+	buildConfig, err := r.loadBuildConfig(ns, jobName, jobUrl)
+	if err != nil {
+		respondError(request, response, err)
+		return
+	}
+	if buildConfig == nil {
+		respondErrorMessage(request, response, fmt.Sprintf("No BuildConfig could be found for job %s", jobName))
+		return
+	}
+	response.WriteEntity(buildConfig)
+}
+
+// loadBuildConfig loads a BuildConfig for a given jobName
+func (r BuildConfigsResource) loadBuildConfig(ns string, jobName string, jobUrl string) (*oapi.BuildConfig, error) {
+	jenkins := r.Jenkins
+	item, err := jenkins.GetJobConfig(jobName)
+	gitUrl := ""
+	if err != nil {
+		return nil, err
+	}
+	mavenJob := item.MavenJobItem
+	pipelineJob := item.PipelineJobItem
+	if mavenJob != nil {
+		//log.Printf("Found maven job: (%+v)", mavenJob)
+		gitUrl = getGitUrlFromScm(mavenJob.Scm)
+	} else if pipelineJob != nil {
+		//log.Printf("Found pipeline job: (%+v)", pipelineJob)
+		gitUrl = getGitUrlFromScm(pipelineJob.Definition.Scm)
+	} else {
+		//log.Printf("Unknown job type (%+v)", item);
+		return nil, nil
+	}
+	return &oapi.BuildConfig{
+		ObjectMeta: kapi.ObjectMeta{
+			Name: jobName,
+			Namespace: ns,
+			Annotations: map[string]string{
+				"fabric8.io/jenkins-url": jobUrl,
+			},
+		},
+		Spec: oapi.BuildConfigSpec{
+			BuildSpec: oapi.BuildSpec{
+				Source: oapi.BuildSource{
+					Type: oapi.BuildSourceGit,
+					Git: &oapi.GitBuildSource{
+						URI: gitUrl,
+					},
+
+				},
+			},
+		},
+	}, nil
+}
+
+func getGitUrlFromScm(scm gojenkins.Scm) string {
+	answer := ""
+	scmContent := scm.ScmContent
+	switch t := scmContent.(type) {
+	case *gojenkins.ScmGit:
+		urls := t.UserRemoteConfigs.UserRemoteConfig.Urls
+		if len(urls) > 0 {
+			answer = urls[0]
+		}
+		if len(answer) == 0 {
+			answer = t.GitBrowser.Url
+		}
+	}
+	return answer
+}
 
